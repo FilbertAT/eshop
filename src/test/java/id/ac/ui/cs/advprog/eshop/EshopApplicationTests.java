aaf45b8979b860b4da81d845902cbcf28f6d6a90
FilbertAT <filbertaurelian@gmail.com>
2025-02-20 12:30:51 +0700

    fix: maintainability issue (Add at least one assertion to this test case to EshopApplicationTests)

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class EshopApplicationTests {
@@ -13,7 +14,7 @@ class EshopApplicationTests {
     @Test
     void mainMethodRuns() {
         // When calling the main method, if no exceptions are thrown, we consider the test passed.
-        EshopApplication.main(new String[] {});
+        assertDoesNotThrow(() -> EshopApplication.main(new String[] {}));
     }
 
 }
