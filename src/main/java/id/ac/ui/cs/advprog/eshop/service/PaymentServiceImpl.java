60510e7e726e335f996c888874de5608c8ab83ef
FilbertAT <filbertaurelian@gmail.com>
2025-03-07 19:17:02 +0700

    [RED] Add PaymentServiceImpl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    OrderService orderService;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData){
+        // Impelement this constructor
+        return null;
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status){
+        // Implement this method
+        return null;
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        // Implement this method
+        return null;
+    }
+    
+    @Override
+    public Payment getPayment(String paymentId) {
+        // Implement this method
+        return null;
+    }
+}
